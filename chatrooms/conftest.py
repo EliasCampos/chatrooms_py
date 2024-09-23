a18224c39d9c0a9abcb0f3b3bfc6004a0407fefe
Illia <camposylia@gmail.com>
2024-09-23 17:59:21 +0300

    Update conftest.py

--- chatrooms/conftest.py
+++ chatrooms/conftest.py
@@ -18,6 +18,13 @@ def event_loop():
     return asyncio.get_event_loop()
 
 
+@pytest.fixture(autouse=True)
+ def fake_db(event_loop):
+     initializer(db_url=settings.DATABASE_URI, modules=settings.APPS_MODELS, loop=event_loop)
+     yield
+     finalizer()
+
+
 @pytest.fixture
 async def async_client():
     async with AsyncClient(app=app, base_url="http://test") as client:
